f7e968d61f0c89c1443da4cc53a53700ff0e33e1
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-10-29 23:00:07 -0400

    I finished this one in time I think...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,35 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("This program will sum the numbers from 1 to n. Please enter a number n:");
+        int n = sc.nextInt();
+        System.out.println("The sum of 1 to " + n + " is " + sumOfNumbers(n));
+        System.out.println("The sum of 1 to " + n + " is " + gaussianSumOfNumbers(n));
     }
-    
-    int sumOfNumbers(int n) {
-        return 0;
+
+
+    static int sumOfNumbers(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        return sum;
     }
-    
+
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+    static int gaussianSumOfNumbers(int n) {
+        int sum2 = (n*(n + 1)) / 2;
+        return sum2;
     }
-   
-    // for Extra extra credit - compare the methods and show which one is faster
-    
+
 }
+// for Extra extra credit - compare the methods and show which one is faster
+
